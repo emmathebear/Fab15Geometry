43edd655890acee89444a1d7b9b57c21301f415e
emmathebear <158595325+emmathebear@users.noreply.github.com>
2024-02-20 15:23:35 +0100

    corretto l'esercizio

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,7 @@
-import java.util.Arrays;
-
 public class Main {
     public static void main(String[] args) {
         Rectangle rectangle = new Rectangle(3,3);
         System.out.println(rectangle);
+        System.out.println(rectangle.calculateArea());
     }
 }
